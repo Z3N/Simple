77e91437ab32226f971cd68f9af36e5e31d68769
Z3N <not@actual.email>
2024-06-27 18:14:02 +0300

    Test

--- src/main/java/com/z3n/simple/HelloServlet.java
+++ src/main/java/com/z3n/simple/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "It's alive! Again...";
+        message = "It's now dead";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
